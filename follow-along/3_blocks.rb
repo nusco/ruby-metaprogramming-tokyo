89c436bfd1beae2d9497092fd95ac669bf185d57
Paolo Perrotta <paolo.nusco.perrotta@gmail.com>
2015-12-11 14:51:58 +0900

    Clean up unused material

---
+++ follow-along/3_blocks.rb
@@ -0,0 +1,47 @@
+# Quiz: Rubython
+
+# blocks are closures
+
+def my_method
+  x = "Goodbye"
+  yield("cruel")
+end
+
+x = "Hello"
+my_method {|y| "#{x}, #{y} world" } # => 
+
+# bindings inside the block disappear after the block
+
+def just_yield
+  yield
+end
+
+top_level_variable = 1
+
+just_yield do
+  top_level_variable += 1
+  local_to_block = 1
+end
+
+top_level_variable # => 
+local_to_block # => 
+
+# scope gates
+
+v1 = 1
+
+class MyClass
+  v2 = 2 # !> assigned but unused variable - v2
+  local_variables # => 
+  
+  def my_method
+    v3 = 3 # !> assigned but unused variable - v3
+    local_variables # => 
+  end
+  
+  local_variables # => 
+end
+
+obj = MyClass.new
+obj.my_method
+local_variables # => 
