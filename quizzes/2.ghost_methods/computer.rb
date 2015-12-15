6820b7e4ca759cb3204af61b8ec04043ee1a7f51
Paolo Perrotta <paolo.nusco.perrotta@gmail.com>
2015-12-15 14:30:00 +0900

    Fix instructions in ghost_methods quiz

--- quizzes/2.ghost_methods/computer.rb
+++ quizzes/2.ghost_methods/computer.rb
@@ -1,3 +1,22 @@
+# This system has three parts:
+# - A "data source" (data_source.rb). Imagine that this file comes from a library,
+#   and you cannot change it.
+# - The Computer class below. It takes information from the data source, and
+#   formats it for a screen-based report.
+# - A unit test for the Computer class (computer_test.rb).
+#
+# Try running the test:
+#
+#  ruby computer_test.rb
+#
+# (If the line that requires 'test/unit' doesn't work on your system, then install
+# the 'test-unit' gem with "gem install test-unit".
+#
+# OK, now for your task. The Computer class contains a lot of duplicated code. Can you
+# remove this duplication by using method_missing()? You should only touch the Computer
+# class - not the test, and not the data_source.rb file. At the end, the duplicated code
+# should be gone, and the test should still be green.
+
 class Computer < BasicObject
   def initialize(computer_id, data_source)
     @id = computer_id
