74c7460f2f0545b1cd3c9eeee9d28e99e1d8c1d0
Paolo Perrotta <paolo.nusco.perrotta@gmail.com>
2015-12-15 14:33:55 +0900

    More more fixing ghost_methods quiz

--- quizzes/2.ghost_methods/computer.rb
+++ quizzes/2.ghost_methods/computer.rb
@@ -17,17 +17,32 @@
 # class - not the test, and not the data_source.rb file. At the end, the duplicated code
 # should be gone, and the test should still be green.
 
-class Computer < BasicObject
+class Computer
   def initialize(computer_id, data_source)
     @id = computer_id
     @data_source = data_source
   end
 
-  def method_missing(name, *args)
-    super if !@data_source.respond_to?("get_#{name}_info")
-    info = @data_source.send("get_#{name}_info", @id)
-    price = @data_source.send("get_#{name}_price", @id)
-    result = "#{name.capitalize}: #{info} ($#{price})"
+  def mouse
+    info = @data_source.get_mouse_info(@id)
+    price = @data_source.get_mouse_price(@id)
+    result = "Mouse: #{info} ($#{price})"
+    return "* #{result}" if price >= 100
+    result
+  end
+
+  def cpu
+    info = @data_source.get_cpu_info(@id)
+    price = @data_source.get_cpu_price(@id)
+    result = "Cpu: #{info} ($#{price})"
+    return "* #{result}" if price >= 100
+    result
+  end
+
+  def keyboard
+    info = @data_source.get_keyboard_info(@id)
+    price = @data_source.get_keyboard_price(@id)
+    result = "Keyboard: #{info} ($#{price})"
     return "* #{result}" if price >= 100
     result
   end
--- quizzes/solutions/2.ghost_methods/computer.rb
+++ quizzes/solutions/2.ghost_methods/computer.rb
@@ -1,29 +1,14 @@
-class Computer
+class Computer < BasicObject
   def initialize(computer_id, data_source)
     @id = computer_id
     @data_source = data_source
   end
 
-  def mouse
-    info = @data_source.get_mouse_info(@id)
-    price = @data_source.get_mouse_price(@id)
-    result = "Mouse: #{info} ($#{price})"
-    return "* #{result}" if price >= 100
-    result
-  end
-
-  def cpu
-    info = @data_source.get_cpu_info(@id)
-    price = @data_source.get_cpu_price(@id)
-    result = "Cpu: #{info} ($#{price})"
-    return "* #{result}" if price >= 100
-    result
-  end
-
-  def keyboard
-    info = @data_source.get_keyboard_info(@id)
-    price = @data_source.get_keyboard_price(@id)
-    result = "Keyboard: #{info} ($#{price})"
+  def method_missing(name, *args)
+    super if !@data_source.respond_to?("get_#{name}_info")
+    info = @data_source.send("get_#{name}_info", @id)
+    price = @data_source.send("get_#{name}_price", @id)
+    result = "#{name.capitalize}: #{info} ($#{price})"
     return "* #{result}" if price >= 100
     result
   end
